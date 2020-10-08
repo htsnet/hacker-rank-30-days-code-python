ec90f3d9b8386aedae9350ec6a5c4328eff4c161
Hamilton Tenório da Silva <hamilton@hts-net.com.br>
2020-10-08 19:40:53 -0300

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,22 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+x = 3
+y = 4.56
+z = "String"
+
+# Read and save an integer, double, and String to your variables.
+x = int(input())
+y = float(input())
+z = str(input())
+
+# Print the sum of both integer variables on a new line.
+print(i + x)
+
+# Print the sum of the double variables on a new line.
+print(d + y)
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s + z)
