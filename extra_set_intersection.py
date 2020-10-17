b04308d92066d7012c93c3ab897b8b9b35ed1fb1
Hamilton Tenório da Silva <hamilton@hts-net.com.br>
2020-10-17 07:56:27 -0300

    Create extra_set_intersection.py

---
+++ extra_set_intersection.py
@@ -0,0 +1,9 @@
+# Set .intersection() Operation
+
+
+n = int(input())
+nNumbers = set(input().split())
+b = int(input())
+bNumbers = set(input().split())
+
+print(len(nNumbers.intersection(bNumbers)))
