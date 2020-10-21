13e287f4fcd082324343e6b867dd0153c241713e
Hamilton Tenório da Silva <hamilton@hts-net.com.br>
2020-10-21 13:43:55 -0300

    Create extra_set_symmetric_difference.py

---
+++ extra_set_symmetric_difference.py
@@ -0,0 +1,9 @@
+# Set .symmetric_difference() Operation
+
+
+e = int(input())
+eNumbers = set(input().split())
+f = int(input())
+fNumbers = set(input().split())
+
+print(len(eNumbers.symmetric_difference(fNumbers)))
