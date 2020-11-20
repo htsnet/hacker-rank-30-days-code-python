d7b967051b6784c0952e5f648c06fec381fb8e98
Hamilton Tenório da Silva <hamilton@hts-net.com.br>
2020-11-20 14:53:39 -0300

    Create extra_triangle_quest2.py

---
+++ extra_triangle_quest2.py
@@ -0,0 +1,9 @@
+#Triangle Quest 2
+
+for i in range(1,int(input())+1):
+     print((10**i//9)**2)  
+     
+     #this solution consider this math situation
+     # 1 * 1 = 1
+     # 11 * 11 = 121
+     # 111 * 111 = 12321
